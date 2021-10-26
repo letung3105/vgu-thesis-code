e34513d3dfe8451f520b734cc64769d2b9813970
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-10-26 14:37:49 +0700

    new population data

--- Covid19ModelVN/src/PopulationData.jl
+++ Covid19ModelVN/src/PopulationData.jl
@@ -1,49 +1,51 @@
 module PopulationData
 
-using DataFrames
+using DataDeps, DataFrames
 import GeoDataFrames, CSV
 
-function save_vietnam_gadm1_population(
-    vn_adm_gpkg_fpath::AbstractString,
-    gso_population_en_fpath::AbstractString,
-    fdir::AbstractString,
-    fid::AbstractString;
-    recreate::Bool = false,
-)
-    fpath = joinpath(fdir, "$fid.csv")
-    # file exists and don't need to be updated
-    if isfile(fpath) && !recreate
-        return CSV.read(fpath, DataFrame)
-    end
-    # create containing folder if not exists
-    if !isdir(fdir)
-        mkpath(fdir)
-    end
-
-    # read the Geopackage (GADM v2.8)
-    df_vn_adm = unique(GeoDataFrames.read(vn_adm_gpkg_fpath, 1))
-    select!(
-        df_vn_adm,
-        :NAME_1 => :gadm1_name,
-        :VARNAME_1 => :gadm1_varname,
-        :ID_1 => :gadm1_id,
+function __init__()
+    register(
+        DataDep(
+            "gadm2.8",
+            """
+            Dataset: General Administrative Division Map
+            Website: https://gadm.org
+            """,
+            [
+                "https://biogeo.ucdavis.edu/data/gadm2.8/gpkg/USA_adm_gpkg.zip",
+                "https://biogeo.ucdavis.edu/data/gadm2.8/gpkg/VNM_adm_gpkg.zip",
+                "https://biogeo.ucdavis.edu/data/gadm2.8/gpkg/UMI_adm_gpkg.zip",
+            ],
+            post_fetch_method = unpack,
+        ),
     )
-
-    # GSO's expected population for 2020 (EN version)
-    df_vn_population = CSV.read(gso_population_en_fpath, DataFrame)
-    # remove double spaces in names
-    select!(
-        df_vn_population,
-        "Cities, provincies" => :gadm1_varname,
-        "2020 (*) Average population (Thous. pers.)" => :avg_population_thousands,
+    register(
+        DataDep(
+            "gso",
+            """
+            Dataset: Vietnam General Statistic Office Average Population 2020
+            Website: https://gso.gov.vn
+            """,
+            [
+                "https://github.com/letung3105/vgu-thesis-datasets/raw/master/gso/vietnam-2020-average-population-by-province.csv"
+            ],
+        ),
     )
-    # rename the province so it matches data from GADM
-    replace!(df_vn_population.gadm1_varname, "Dak Nong" => "Dac Nong")
-    transform!(
-        df_vn_population,
-        :gadm1_varname => x -> join.(split.(x), " "),
-        :avg_population_thousands => (x -> 1000 .* x) => :avg_population,
-        renamecols = false,
+    return nothing
+end
+
+function combine_vietnam_province_level_gadm_and_gso_population(df_gadm, df_population)
+    # remove rows of repeated provinces
+    df_gadm = unique(df_gadm, :ID_1)
+    # rename columns and transform data
+    df_population = transform(
+        df_population,
+        # remove double spaces in names
+        "Cities, provincies" => (x -> join.(split.(x), " ")) => :VARNAME_1,
+        # times 1000 because the original dataset unit is in "thousands people"
+        "2020 (*) Average population (Thous. pers.)" =>
+            (x -> 1000 .* x) => :AVGPOPULATION,
+        copycols = false,
     )
 
     # remove all spaces in names and make them lowercase
@@ -51,20 +53,21 @@ function save_vietnam_gadm1_population(
     # x is the same as y if the standardized version of y contains the
     # standardized version of x
     issame(x, y) = contains(standardize(y), standardize(x))
-    # check if the name is a province or a city defined by GADM
-    is_in_gadm(s, names) = any(map(name -> issame(s, name), names))
 
-    # remove extra data from GSO
-    filter!(:gadm1_varname => x -> is_in_gadm(x, df_vn_adm.gadm1_varname), df_vn_population)
+    # rename the province so it matches data from GADM
+    replace!(df_population.VARNAME_1, "Dak Nong" => "Dac Nong")
     # use provinces/cities names from GADM
     replace!(
-        varname -> first(filter(x -> issame(varname, x), df_vn_adm.gadm1_varname)),
-        df_vn_population.gadm1_varname,
+        x -> begin
+            name = filter(y -> issame(x, y), df_gadm.VARNAME_1)
+            return isempty(name) ? x : first(name)
+        end,
+        df_population.VARNAME_1,
     )
 
     # get final table
-    df = innerjoin(df_vn_adm, df_vn_population, on = :gadm1_varname)
-    CSV.write(fpath, df)
+    df = innerjoin(df_gadm, df_population, on = :VARNAME_1)
+    select!(df, [:ID_1, :NAME_1, :VARNAME_1, :AVGPOPULATION])
     return df
 end
 
