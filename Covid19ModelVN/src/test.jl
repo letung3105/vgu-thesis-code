c5f9423af3dce876530da1d8049fb7423be33f7f
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-10-26 19:44:22 +0700

    cache data functions

--- Covid19ModelVN/src/test.jl
+++ Covid19ModelVN/src/test.jl
@@ -4,186 +4,187 @@ if isfile("Project.toml") && isfile("Manifest.toml")
     Pkg.activate(".")
 end
 
-using Dates,
-    Plots, DataDeps, DataFrames, CSV, Covid19ModelVN.Datasets, Covid19ModelVN.Helpers
+using Dates, Plots, DataDeps, DataFrames, CSV, Covid19ModelVN.Helpers
 
-import Covid19ModelVN.JHUCSSEData,
-    Covid19ModelVN.FacebookData, Covid19ModelVN.PopulationData, Covid19ModelVN.VnExpressData, Covid19ModelVN.VnCdcData
+import GeoDataFrames,
+    Covid19ModelVN.JHUCSSEData,
+    Covid19ModelVN.FacebookData,
+    Covid19ModelVN.PopulationData,
+    Covid19ModelVN.VnExpressData,
+    Covid19ModelVN.VnCdcData
 
-const DEFAULT_DATASETS_DIR = "datasets"
+const CACHE_DIR = ".cacheddata"
 
-function test_load_datasets()
-    DEFAULT_VIETNAM_GADM1_POPULATION_DATASET(DEFAULT_DATASETS_DIR)
-    DEFAULT_VIETNAM_COVID_DATA_TIMESERIES(DEFAULT_DATASETS_DIR)
-    DEFAULT_VIETNAM_PROVINCE_CONFIRMED_AND_DEATHS_TIMESERIES(
-        DEFAULT_DATASETS_DIR,
-        "HoChiMinh",
+const FPATH_PROVINCES_POPULATION_WITH_GADM_ID_VIETNAM =
+    joinpath(CACHE_DIR, "average-population-by-province-vietnam.csv")
+
+const FPATH_COVID_TIMESERIES_VIETNAM = joinpath(CACHE_DIR, "timeseries-covid19-vietnam.csv")
+
+const FPATH_FB_SOCIAL_CONNECTEDNESS_VIETNAM =
+    joinpath(CACHE_DIR, "social-connectedness-vietnam.csv")
+
+const FPATH_FB_MOVEMENT_RANGE_AVERAGE_VIETNAM =
+    joinpath(CACHE_DIR, "movement-range-vietnam.csv")
+
+const FPATH_FB_MOVEMENT_RANGE_AVERAGE_HCM_CITY =
+    joinpath(CACHE_DIR, "movement-range-hcm-city.csv")
+
+const FPATH_FB_MOVEMENT_RANGE_AVERAGE_BINH_DUONG =
+    joinpath(CACHE_DIR, "movement-range-binh-duong.csv")
+
+const FPATH_FB_MOVEMENT_RANGE_AVERAGE_DONG_NAI =
+    joinpath(CACHE_DIR, "movement-range-dong-nai.csv")
+
+const FPATH_FB_MOVEMENT_RANGE_AVERAGE_LONG_AN =
+    joinpath(CACHE_DIR, "movement-range-long-an.csv")
+
+function cache_vietnam_province_level_gadm_and_gso_population(fpath; recreate = false)
+    if isfile(fpath) && !recreate
+        return fpath
+    end
+
+    df_gadm = GeoDataFrames.read(datadep"gadm2.8/VNM_adm.gpkg", 1)
+    df_population =
+        CSV.read(datadep"gso/vietnam-2020-average-population-by-province.csv", DataFrame)
+
+    df_combined = PopulationData.combine_vietnam_province_level_gadm_and_gso_population(
+        df_gadm,
+        df_population,
     )
-    DEFAULT_VIETNAM_PROVINCE_CONFIRMED_AND_DEATHS_TIMESERIES(
-        DEFAULT_DATASETS_DIR,
-        "BinhDuong",
-    )
-    DEFAULT_VIETNAM_PROVINCE_CONFIRMED_AND_DEATHS_TIMESERIES(
-        DEFAULT_DATASETS_DIR,
-        "DongNai",
-    )
-    DEFAULT_VIETNAM_PROVINCE_CONFIRMED_AND_DEATHS_TIMESERIES(DEFAULT_DATASETS_DIR, "LongAn")
-
-    DEFAULT_VIETNAM_AVERAGE_MOVEMENT_RANGE(DEFAULT_DATASETS_DIR)
-    DEFAULT_VIETNAM_PROVINCE_AVERAGE_MOVEMENT_RANGE(DEFAULT_DATASETS_DIR, 26)
-    DEFAULT_VIETNAM_INTRA_CONNECTEDNESS_INDEX(DEFAULT_DATASETS_DIR)
-    DEFAULT_VIETNAM_SOCIAL_PROXIMITY_TO_CASES_INDEX(DEFAULT_DATASETS_DIR)
-
-    return nothing
+    return save_dataframe(df_combined, fpath)
 end
 
-function test_visualizations()
-    train_range = Day(31)
-    forecast_range = Day(28)
-
-    df_covid = DEFAULT_VIETNAM_PROVINCE_CONFIRMED_AND_DEATHS_TIMESERIES(
-        DEFAULT_DATASETS_DIR,
-        "HoChiMinh",
-    )
-    first_date = first(filter!(x -> x.confirmed_total >= 500, df_covid).date)
-    split_date = first_date + train_range
-    last_date = split_date + forecast_range
-
-    covid_cols = [:deaths_total, :confirmed_total]
-    moving_average!(df_covid, covid_cols, 7)
-
-    train_dataset, test_dataset =
-        train_test_split(df_covid, covid_cols, :date, first_date, split_date, last_date)
-    plt = plot(title = "covid cases", legend = :outertop)
-    plot!(
-        [train_dataset.data test_dataset.data]',
-        labels = ["deaths total" "confirmed total"],
-    )
-    display(plt)
-
-    df_population = DEFAULT_VIETNAM_GADM1_POPULATION_DATASET(DEFAULT_DATASETS_DIR)
-    province_id =
-        first(filter(x -> x.gadm1_name == "Hồ Chí Minh city", df_population).gadm1_id)
-
-    # load facebook movement range
-    df_movement_range = DEFAULT_VIETNAM_AVERAGE_MOVEMENT_RANGE(DEFAULT_DATASETS_DIR)
-    movement_range_cols =
-        [:all_day_bing_tiles_visited_relative_change, :all_day_ratio_single_tile_users]
-    moving_average!(df_movement_range, movement_range_cols, 7)
-    # load timeseries data with the chosen temporal lag
-    load_movement_range(lag) = load_timeseries(
-        df_movement_range,
-        movement_range_cols,
-        :ds,
-        first_date - lag,
-        last_date - lag,
-    )
-    movement_data = load_movement_range(Day(2))
-    plt = plot(title = "Movement Range Index", legend = :outertop)
-    plot!(movement_data, labels = ["relative movement change" "relative stay put ratio"])
-    display(plt)
-
-    df_vn_spc = DEFAULT_VIETNAM_SOCIAL_PROXIMITY_TO_CASES_INDEX(DEFAULT_DATASETS_DIR)
-    moving_average!(df_vn_spc, "Hồ Chí Minh city", 7)
-    load_spc(lag) = load_timeseries(
-        df_vn_spc,
-        "Hồ Chí Minh city",
-        :date,
-        first_date - lag,
-        first_date - lag + train_range + forecast_range,
-    )
-    vn_spc_data = load_spc(Day(2))
-    plt = plot(title = "Social Proximity to Cases Index", legend = :outertop)
-    plot!(vn_spc_data)
-    display(plt)
-
-    nothing
-end
-
-function test_save_jhu_csse_covid_timeseries_global()
+function cache_jhu_csse_covid_timeseries_country_level(
+    fpaths,
+    country_names;
+    recreate = false,
+)
     df_confirmed =
         CSV.read(datadep"jhu-csse/time_series_covid19_confirmed_global.csv", DataFrame)
     df_recovered =
         CSV.read(datadep"jhu-csse/time_series_covid19_recovered_global.csv", DataFrame)
     df_deaths = CSV.read(datadep"jhu-csse/time_series_covid19_deaths_global.csv", DataFrame)
 
-    @time df = JHUCSSEData.combine_country_level_timeseries(
-        df_confirmed,
-        df_recovered,
-        df_deaths,
-        "Vietnam",
-    )
-    save_dataframe(df, "datasets/jhu-csse", "time_series_covid19_combined_vietnam")
+    for (fpath, country_name) ∈ zip(fpaths, country_names)
+        if isfile(fpath) && !recreate
+            continue
+        end
+
+        df_combined = JHUCSSEData.combine_country_level_timeseries(
+            df_confirmed,
+            df_recovered,
+            df_deaths,
+            country_name,
+        )
+        save_dataframe(df_combined, fpath)
+    end
+
+    return nothing
 end
 
-function test_save_jhu_csse_covid_timeseries_US()
+function cache_jhu_csse_covid_timeseries_us_county_level(
+    fpaths,
+    state_names,
+    county_names;
+    recreate = false,
+)
     df_confirmed =
         CSV.read(datadep"jhu-csse/time_series_covid19_confirmed_US.csv", DataFrame)
     df_deaths = CSV.read(datadep"jhu-csse/time_series_covid19_deaths_US.csv", DataFrame)
 
-    @time population = JHUCSSEData.get_us_county_population(df_deaths, "Alabama", "Autauga")
-    @show population
+    for (fpath, state_name, county_name) ∈ zip(fpaths, state_names, county_names)
+        if isfile(fpath) && !recreate
+            continue
+        end
 
-    @time df = JHUCSSEData.combine_us_county_level_timeseries(
-        df_confirmed,
-        df_deaths,
-        "Alabama",
-        "Autauga",
-    )
-    save_dataframe(df, "datasets/jhu-csse", "time_series_covid19_combined_us_autauga")
+        df_combined = JHUCSSEData.combine_us_county_level_timeseries(
+            df_confirmed,
+            df_deaths,
+            state_name,
+            county_name,
+        )
+        save_dataframe(df_combined, fpath)
+    end
+
+    return nothing
 end
 
-function test_save_fb_movement_range()
+function cache_fb_movement_range_timeseries_region_average(
+    fpaths,
+    country_codes,
+    subdivision_ids;
+    recreate = false,
+)
     df_movement_range =
         FacebookData.read_movement_range(datadep"facebook/movement-range-2021-10-09.txt")
 
-    @time df_movement_range_vn =
-        FacebookData.region_average_movement_range(df_movement_range, "VNM")
-    save_dataframe(
-        df_movement_range_vn,
-        "datasets/facebook",
-        "facebook-movement-range-average-vnm",
-    )
+    for (fpath, country_code, subdivision_id) ∈ zip(fpaths, country_codes, subdivision_ids)
+        if isfile(fpath) && !recreate
+            return fpath
+        end
+
+        df_region_movement_range = FacebookData.region_average_movement_range(
+            df_movement_range,
+            country_code,
+            subdivision_id,
+        )
+        save_dataframe(df_region_movement_range, fpath)
+    end
+
+    return nothing
 end
 
-function test_save_fb_social_connectedness()
+function cache_fb_social_connectedness_inter_province(
+    fpaths,
+    country_codes;
+    recreate = false,
+)
     df_social_connectedness = FacebookData.read_social_connectedness(
-        datadep"facebook/gadm1_nuts2_gadm1_nuts2_aug2020.tsv",
+        datadep"facebook/gadm1_nuts2_gadm1_nuts2.tsv",
     )
 
-    @time df_social_connectedness_vn =
-        FacebookData.inter_province_social_connectedness(df_social_connectedness, "VNM")
-    save_dataframe(
-        df_social_connectedness_vn,
-        "datasets/facebook",
-        "facebook-social-connectedness-vnm",
-    )
-end
+    for (fpath, country_code) ∈ zip(fpaths, country_codes)
+        if isfile(fpath) && !recreate
+            return fpath
+        end
 
-function test_read_vnexpress_covid_timeseries_vietnam_provinces()
-    df1 = CSV.read(datadep"vnexpress/timeseries-vietnam-provinces-confirmed.csv", DataFrame)
-    df2 = CSV.read(datadep"vnexpress/timeseries-vietnam-provinces-confirmed-total.csv", DataFrame)
-
-    @show df1
-    @show df2
+        df_country_social_connectedness = FacebookData.inter_province_social_connectedness(
+            df_social_connectedness,
+            country_code,
+        )
+        save_dataframe(df_country_social_connectedness, fpath)
+    end
 
     return nothing
 end
 
-
-function test_parse_vncdc_covid_timeseries_vietnam_provinces()
-    df1 = VnCdcData.parse_timeseries_confirmed_and_deaths(datadep"vncdc/HoChiMinh.json")
-    df2 = VnCdcData.parse_timeseries_confirmed_and_deaths(datadep"vncdc/BinhDuong.json")
-    df3 = VnCdcData.parse_timeseries_confirmed_and_deaths(datadep"vncdc/DongNai.json")
-    df4 = VnCdcData.parse_timeseries_confirmed_and_deaths(datadep"vncdc/LongAn.json")
-
-    @show df1
-    @show df2
-    @show df3
-    @show df4
-
-    return nothing
+function cache_default_data(; recreate = false)
+    cache_vietnam_province_level_gadm_and_gso_population(
+        FPATH_PROVINCES_POPULATION_WITH_GADM_ID_VIETNAM,
+        recreate = recreate,
+    )
+    cache_jhu_csse_covid_timeseries_country_level(
+        [FPATH_COVID_TIMESERIES_VIETNAM],
+        ["Vietnam"],
+        recreate = recreate,
+    )
+    cache_fb_social_connectedness_inter_province(
+        [FPATH_FB_SOCIAL_CONNECTEDNESS_VIETNAM],
+        ["VNM"],
+        recreate = recreate,
+    )
+    cache_fb_movement_range_timeseries_region_average(
+        [
+            FPATH_FB_MOVEMENT_RANGE_AVERAGE_VIETNAM,
+            FPATH_FB_MOVEMENT_RANGE_AVERAGE_HCM_CITY,
+            FPATH_FB_MOVEMENT_RANGE_AVERAGE_BINH_DUONG,
+            FPATH_FB_MOVEMENT_RANGE_AVERAGE_DONG_NAI,
+            FPATH_FB_MOVEMENT_RANGE_AVERAGE_LONG_AN,
+        ],
+        ["VNM", "VNM", "VNM", "VNM", "VNM"],
+        [nothing, 26, 10, 2, 39],
+        recreate = recreate,
+    )
 end
 
-test_read_vnexpress_covid_timeseries_vietnam_provinces()
-test_parse_vncdc_covid_timeseries_vietnam_provinces()
+cache_default_data()
