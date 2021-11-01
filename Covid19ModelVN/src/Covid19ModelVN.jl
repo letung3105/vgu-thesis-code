bbf0c97c58b22b416a6671cc939c652727e53b23
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-11-01 17:44:09 +0700

    add links to prebuilt datasets

--- Covid19ModelVN/src/Covid19ModelVN.jl
+++ Covid19ModelVN/src/Covid19ModelVN.jl
@@ -1,5 +1,31 @@
 module Covid19ModelVN
 
+using DataDeps
+
+function __init__()
+    register(
+        DataDep(
+            "covid19model",
+            """
+            Dataset: Population and Social Proximty to Cases
+            """,
+            [
+                "https://github.com/letung3105/coviddata/raw/master/covid19model/average-population-vn-provinces.csv",
+                "https://github.com/letung3105/coviddata/raw/master/covid19model/average-population-us-counties.csv",
+                "https://github.com/letung3105/coviddata/raw/master/covid19model/social-proximity-to-cases-vn-provinces.csv",
+                "https://github.com/letung3105/coviddata/raw/master/covid19model/social-proximity-to-cases-us-counties.csv",
+                "https://github.com/letung3105/coviddata/raw/master/covid19model/movement-range-vietnam.csv",
+                "https://github.com/letung3105/coviddata/raw/master/covid19model/movement-range-hcm-city.csv",
+                "https://github.com/letung3105/coviddata/raw/master/covid19model/movement-range-binh-duong.csv",
+                "https://github.com/letung3105/coviddata/raw/master/covid19model/movement-range-dong-nai.csv",
+                "https://github.com/letung3105/coviddata/raw/master/covid19model/movement-range-long-an.csv",
+                "https://github.com/letung3105/coviddata/raw/master/covid19model/timeseries-covid19-combined-vietnam.csv",
+            ],
+        ),
+    )
+    return nothing
+end
+
 include("helpers.jl")
 include("models.jl")
 include("train_eval.jl")
