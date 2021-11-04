f1e5b3bb427162cff029aca57331f539232169d6
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-11-04 23:39:43 +0700

    refactor helpers

--- Covid19ModelVN/src/helpers.jl
+++ Covid19ModelVN/src/helpers.jl
@@ -1,4 +1,5 @@
 export TimeseriesDataset,
+    TimeseriesConfig,
     train_test_split,
     load_timeseries,
     save_dataframe,
@@ -11,6 +12,23 @@ export TimeseriesDataset,
     moving_average!
 
 using Dates, Statistics, CSV, DataFrames
+
+struct TimeseriesConfig
+    df::AbstractDataFrame
+    date_col::Union{Symbol,<:AbstractString}
+    data_cols::Union{
+        Symbol,
+        <:AbstractString,
+        <:AbstractVector{Symbol},
+        <:AbstractVector{<:AbstractString},
+    }
+end
+
+function load_timeseries(config::TimeseriesConfig, first_date::Date, last_date::Date)
+    df = bound(config.df, config.date_col, first_date, last_date, view = true)
+    data = Array(df[!, Cols(config.data_cols)])
+    return data'
+end
 
 """
 This contains the minimum required information for a timeseriese dataset that is used by UDEs
@@ -27,77 +45,23 @@ struct TimeseriesDataset
     tsteps::Union{<:Real,AbstractVector{<:Real},StepRange,StepRangeLen}
 end
 
-"""
-Create two `TimeseriesDataset`s from the given dataframe, the first dataset contains data point whose `date_col`
-value is in the range [first_date, split_date], and the second dataset contains data point whose `date_col`
-value is in the range (split_date, last_date]
-
-# Arguments
-
-+ `df`: The dataframe
-+ `data_cols`: The names of the columns whose data will be used for creating an array
-+ `date_col`: The name of the column that contains the date of the data point
-+ `first_date`: First date to take
-+ `split_date`: Date where to dataframe is splitted in two
-+ `last_date`: Last date to take
-"""
 function train_test_split(
-    df::AbstractDataFrame,
-    data_cols::Union{
-        AbstractVector{<:AbstractString},
-        AbstractVector{Symbol},
-        <:AbstractString,
-        Symbol,
-    },
-    date_col::Union{<:AbstractString,Symbol},
+    config::TimeseriesConfig,
     first_date::Date,
     split_date::Date,
     last_date::Date,
 )
-    df_train = bound(df, date_col, first_date, split_date)
-    df_test = bound(df, date_col, split_date + Day(1), last_date)
-
+    train_data = load_timeseries(config, first_date, split_date)
     train_tspan = Float64.((0, Dates.value(split_date - first_date)))
-    test_tspan = Float64.((0, Dates.value(last_date - first_date)))
-
     train_tsteps = train_tspan[1]:1:train_tspan[2]
-    test_tsteps = (train_tspan[2]+1):1:test_tspan[2]
-
-    train_data = Float64.(Array(df_train[!, data_cols])')
-    test_data = Float64.(Array(df_test[!, data_cols])')
-
     train_dataset = TimeseriesDataset(train_data, train_tspan, train_tsteps)
+
+    test_data = load_timeseries(config, split_date + Day(1), last_date)
+    test_tspan = Float64.((0, Dates.value(last_date - first_date)))
+    test_tsteps = (train_tspan[2]+1):1:test_tspan[2]
     test_dataset = TimeseriesDataset(test_data, test_tspan, test_tsteps)
 
     return train_dataset, test_dataset
-end
-
-"""
-Load from the time series in the dataframe the data from `data_cols` columns, limiting
-the data point `date_col` between [`first_date`, `last_date`].
-
-# Arguments
-
-+ `df`: The dataframe
-+ `data_cols`: The names of the columns whose data will be used for creating an array
-+ `date_col`: The name of the column that contains the date of the data point
-+ `first_date`: First date to take
-+ `last_date`: Last date to take
-"""
-function load_timeseries(
-    df::AbstractDataFrame,
-    data_cols::Union{
-        AbstractVector{<:AbstractString},
-        AbstractVector{Symbol},
-        <:AbstractString,
-        Symbol,
-    },
-    date_col::Union{<:AbstractString,Symbol},
-    first_date::Date,
-    last_date::Date,
-)
-    df = bound(df, date_col, first_date, last_date)
-    return Array(df[!, Cols(data_cols)])
 end
 
 """
@@ -163,8 +127,13 @@ Select a subset of the dataframe `df` such that values in `col` remain between `
 + `first`: The starting (smallest) value allowed
 + `last`: The ending (largest) value allowed
 """
-bound(df::AbstractDataFrame, col::Union{<:AbstractString,Symbol}, first::Any, last::Any) =
-    subset(df, col => x -> (x .>= first) .& (x .<= last), view = true)
+bound(
+    df::AbstractDataFrame,
+    col::Union{Symbol,<:AbstractString},
+    first::Any,
+    last::Any;
+    kwargs...,
+) = subset(df, col => x -> (x .>= first) .& (x .<= last); kwargs...)
 
 """
 Filter the dataframe `df` such that values in `col` remain between `start_date` and `end_date`
@@ -176,8 +145,13 @@ Filter the dataframe `df` such that values in `col` remain between `start_date` 
 + `first`: The starting (smallest) value allowed
 + `last`: The ending (largest) value allowed
 """
-bound!(df::AbstractDataFrame, col::Union{<:AbstractString,Symbol}, first::Any, last::Any) =
-    subset!(df, col => x -> (x .>= first) .& (x .<= last))
+bound!(
+    df::AbstractDataFrame,
+    col::Union{Symbol,<:AbstractString},
+    first::Any,
+    last::Any;
+    kwargs...,
+) = subset!(df, col => x -> (x .>= first) .& (x .<= last); kwargs...)
 
 """
 Calculate the moving average of the given list of numbers
@@ -202,10 +176,10 @@ Calculate the moving average of all the `cols` in `df`
 moving_average!(
     df::AbstractDataFrame,
     cols::Union{
-        <:AbstractVector{<:AbstractString},
-        <:AbstractVector{Symbol},
-        <:AbstractString,
         Symbol,
+        <:AbstractString,
+        <:AbstractVector{Symbol},
+        <:AbstractVector{<:AbstractString},
     },
     n::Integer,
 ) = transform!(df, names(df, Cols(cols)) .=> x -> moving_average(x, n), renamecols = false)
