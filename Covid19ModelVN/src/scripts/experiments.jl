02f0fd72049432870da5cf9c4ecea5d808216ffc
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-11-13 06:52:47 +0000

    fbmobility3 setup function

--- Covid19ModelVN/src/scripts/experiments.jl
+++ Covid19ModelVN/src/scripts/experiments.jl
@@ -1,9 +1,3 @@
-# Activate the environment for running the script
-if isfile("Project.toml") && isfile("Manifest.toml")
-    import Pkg
-    Pkg.activate(".")
-end
-
 using Dates, Statistics, Serialization
 using OrdinaryDiffEq, DiffEqFlux
 using CairoMakie
@@ -304,6 +298,53 @@ function setup_fbmobility3(loc::AbstractString, hyperparams::SEIRDFbMobility3Hyp
 
     # build the model
     model = SEIRDFbMobility3(
+        hyperparams.γ_bounds,
+        hyperparams.λ_bounds,
+        hyperparams.α_bounds,
+        movement_range_data,
+        social_proximity_data,
+    )
+    # get the initial states and available observations depending on the model type
+    # and the considered location
+    u0, vars, labels = experiment_SEIRD_initial_states(loc, train_dataset.data[:, 1])
+    p0 = initparams(model, hyperparams.γ0, hyperparams.λ0)
+    lossfn = experiment_loss(train_dataset.tsteps, hyperparams.ζ)
+    return model, u0, p0, lossfn, train_dataset, test_dataset, vars, labels
+end
+
+SEIRDFbMobility4Hyperparams = @NamedTuple begin
+    ζ::Float64
+    γ0::Float64
+    λ0::Float64
+    γ_bounds::Tuple{Float64,Float64}
+    λ_bounds::Tuple{Float64,Float64}
+    α_bounds::Tuple{Float64,Float64}
+    train_range::Day
+    forecast_range::Day
+    social_proximity_lag::Day
+end
+
+function setup_fbmobility4(loc::AbstractString, hyperparams::SEIRDFbMobility4Hyperparams)
+    # get data for model
+    train_dataset, test_dataset, first_date, last_date =
+        experiment_covid19_data(loc, hyperparams.train_range, hyperparams.forecast_range)
+    @assert size(train_dataset.data, 2) == Dates.value(hyperparams.train_range)
+    @assert size(test_dataset.data, 2) == Dates.value(hyperparams.forecast_range)
+
+    movement_range_data = experiment_movement_range(loc, first_date, last_date)
+    @assert size(movement_range_data, 2) ==
+            Dates.value(hyperparams.train_range) + Dates.value(hyperparams.forecast_range)
+
+    social_proximity_data = experiment_social_proximity(
+        loc,
+        first_date - hyperparams.social_proximity_lag,
+        last_date - hyperparams.social_proximity_lag,
+    )
+    @assert size(social_proximity_data, 2) ==
+            Dates.value(hyperparams.train_range) + Dates.value(hyperparams.forecast_range)
+
+    # build the model
+    model = SEIRDFbMobility4(
         hyperparams.γ_bounds,
         hyperparams.λ_bounds,
         hyperparams.α_bounds,
