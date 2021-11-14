98cba10829ae4942cb994b85f8151671239d1985
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-11-14 13:55:51 +0000

    use HyperOpt

--- Covid19ModelVN/src/scripts/batchjob_fbmobility5.jl
+++ Covid19ModelVN/src/scripts/batchjob_fbmobility5.jl
@@ -1,39 +1,44 @@
 include("cmd.jl")
 
-let
-    locations = [
-        collect(keys(Covid19ModelVN.LOC_NAMES_VN))
-        collect(keys(Covid19ModelVN.LOC_NAMES_US))
-    ]
-    args_list = [
-        [
-            "--locations",
-            locations...,
-            "--savedir",
-            "snapshots/batchjob_test",
-            "fbmobility5_A",
+using Hyperopt
+
+ho = let
+    loc = "losangeles_ca"
+    @thyperopt for i ∈ 50, # number of samples
+        sampler ∈ RandomSampler(), # choose an parameters sampler
+        ζ ∈ [exp10.(-3:-1); exp10.(-3:-1) .* 5],
+        adam_lr ∈ [exp10.(-4:-2); exp10.(-4:-2) .* 5],
+        adam_maxiters ∈ exp10.(2:4) .* 5,
+        bfgs_initial_stepnorm ∈ exp10.(-3:-2),
+        bfgs_maxiters ∈ exp10.(2:4)
+
+        _, final_loss = experiment_train(
             "fbmobility5",
-        ],
-        [
-            "--locations",
-            locations...,
-            "--savedir=snapshots/batchjob_test",
-            "--adam-lr=0.015",
-            "--adam-maxiters=500",
-            "fbmobility5_B",
-            "fbmobility5",
-        ],
-        [
-            "--locations",
-            locations...,
-            "--savedir=snapshots/batchjob_test",
-            "--adam-lr=0.001",
-            "--adam-maxiters=1000",
-            "fbmobility5_B",
-            "fbmobility5",
-        ]
-    ]
-    @sync for args ∈ args_list
-        @async runcmd(args)
+            () -> setup_fbmobility5(
+                loc,
+                (
+                    ζ = ζ,
+                    γ0 = 1 / 3,
+                    λ0 = 1 / 14,
+                    β_bounds = (0.0, 1.336),
+                    γ_bounds = (1 / 5, 1 / 2),
+                    λ_bounds = (1 / 21, 1 / 7),
+                    α_bounds = (0.0, 0.06),
+                    train_range = Day(32),
+                    forecast_range = Day(28),
+                    social_proximity_lag = Day(14),
+                ),
+            ),
+            TrainConfig[
+                TrainConfig("ADAM", ADAM(adam_lr), adam_maxiters),
+                TrainConfig(
+                    "BFGS",
+                    BFGS(initial_stepnorm = bfgs_initial_stepnorm),
+                    bfgs_maxiters,
+                ),
+            ],
+            "snapshots/fbmobility5/hyperopt",
+        )
+        final_loss
     end
 end
